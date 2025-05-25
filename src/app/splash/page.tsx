425c80a36715ba08da18a76199fc49a347648575
MinJi Koh <36044398+spiritstone@users.noreply.github.com>
2025-05-25 23:14:23 +0900

    fix

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -1,7 +1,6 @@
+"use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-
-("use client");
 
 export default function SplashPage() {
   const router = useRouter();

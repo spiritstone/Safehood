2d3fe84a4885679d33417bf8e68123d4379c5312
MinJi Koh <36044398+spiritstone@users.noreply.github.com>
2025-05-25 22:28:50 +0900

    splash page

---
+++ src/app/splash/page.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+("use client");
+
+export default function SplashPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 2000);
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, [router]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Welcome to Safehood</h1>
+    </div>
+  );
+}

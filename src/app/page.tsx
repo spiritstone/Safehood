2121162f8d26c14fa0c7b0b35c55aaae7d50d7c7
MinJi Koh <36044398+spiritstone@users.noreply.github.com>
2025-05-26 21:45:52 +0900

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import Script from "next/script";
 
 export default function Home() {
